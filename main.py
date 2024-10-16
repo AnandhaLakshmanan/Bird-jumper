9c0e52a08c62e65915cee58017a2496986dec428
Anand <anandhalakshmanan2001@gmail.com>
2024-10-16 15:42:54 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import pygame
-from pygame import display, font, image, mixer, mouse, sprite, time, transform
-from pygame import event as events
+from pygame import display, font, image, mixer, mouse, sprite, time, transform, event as events
 from random import SystemRandom
 
 
 pygame.init()
-mixer.init()
 
 clock = time.Clock()
 random_generator = SystemRandom()
@@ -21,8 +19,8 @@ PIPE_GAP = 150
 PIPE_FREQUENCY = 1500  # milliseconds
 
 # Set game window dimensions and caption
-screen = display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-display.set_caption("Flappy Bird")
+screen = display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+display.set_caption(title="Flappy Bird")
 
 # Game icon image
 icon_img = image.load("Resources/icon.png")
@@ -67,43 +65,42 @@ def load_high_score():
         return 0
 
 
-def render_score_and_high_score(score, high_score):
+def render_score_and_high_score(current_score, current_high_score):
     """
     Renders the current score and the highest score on the game screen.
 
-    :param score: The player's current score (int) during gameplay.
-    :param high_score: The highest score achieved by the player (int) across all sessions.
+    :param current_score: The player's current score (int) during gameplay.
+    :param current_high_score: The highest score achieved by the player (int) across all sessions.
     """
-    high_score_style = font.Font(None, 40)
-    score_style = font.SysFont("Bauhaus 93", 60)
-    high_score_txt = high_score_style.render(f"High Score: {high_score}", True, (255, 255, 255))
-    score_txt = score_style.render(f"{score}", True, (255, 255, 255))
+    high_score_style = font.Font(size=40)
+    score_style = font.SysFont(name="Bauhaus 93", size=60)
+    high_score_txt = high_score_style.render(f"High Score: {current_high_score}", True, (255, 255, 255))
+    score_txt = score_style.render(f"{current_score}", True, (255, 255, 255))
     screen.blit(high_score_txt, (SCREEN_WIDTH_HALF + 250, 40))
     screen.blit(score_txt, (SCREEN_WIDTH_HALF, 20))
 
 
-def save_high_score(high_score):
+def save_high_score(current_high_score):
     """
     Saves the high score to a text file ('high_score.txt').
 
-    :param high_score: The highest score (int) that should be saved.
+    :param current_high_score: The highest score (int) that should be saved.
     """
     with open("high_score.txt", "w") as file:
-        file.write(str(high_score))
+        file.write(str(current_high_score))
 
 
 def reset_game():
     """
-    Resets the game state to its initial conditions for a fresh start. This function clears the pipes from the screen, repositions the bird 
-    to its starting point, and resets the score. 
+    Resets the game state to its initial conditions for a fresh start. This function clears the pipes from the screen,
+    repositions the bird to its starting point, and resets the score.
     
     :return: score(int).
     """
     pipe_group.empty()
     bird.rect.x = 100
     bird.rect.y = SCREEN_HEIGHT_HALF
-    score = 0
-    return score
+    return 0
 
 
 def draw_background_and_ground():
@@ -111,54 +108,49 @@ def draw_background_and_ground():
     Draws the game background and the ground on the screen.
     """
     # Draw background
-    screen.blit(background_img, (0, 0))
+    screen.blit(source=background_img, dest=(0, 0))
     # Draw ground
-    screen.blit(ground_img, (ground_scroll, 768))
+    screen.blit(source=ground_img, dest=(ground_scroll, 768))
 
 
-class Element():
-    def __init__(self, x, y, image, scale):
+class Element:
+    def __init__(self, x_coord, y_coord, img_surface, scale_factor):
         """
         Initializes an Element object with position, image, and scaling.
         
-        :param x: X-coordinate for the top-left corner of the element.
-        :param y: Y-coordinate for the top-left corner of the element.
-        :param image: The image surface to display for the element.
-        :param scale: Scaling factor for resizing the image.
+        :param x_coord: X-coordinate for the top-left corner of the element.
+        :param y_coord: Y-coordinate for the top-left corner of the element.
+        :param img_surface: The image surface to display for the element.
+        :param scale_factor: Scaling factor for resizing the image.
         """
-        width = image.get_width()
-        height = image.get_height()
-        self.image = transform.scale(image, (int(width * scale), int(height * scale)))
+        width = img_surface.get_width()
+        height = img_surface.get_height()
+        self.image = transform.scale(surface=img_surface, size=(int(width * scale_factor), int(height * scale_factor)))
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x, y)
+        self.rect.topleft = (x_coord, y_coord)
 
-    def draw(self, screen):
+    def draw(self):
         """
-        Draws the element onto the given screen surface.
-
-        :param screen: The surface where the element will be drawn.
+        Draws the element onto the screen surface.
         """
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
 class Button(Element):
-    def __init__(self, x, y, image, scale):
+    def __init__(self, x_coord, y_coord, img_surface, scale_factor):
         """
         Initializes a Button object, inheriting from Element, with additional click handling.
-        
-        :param x: X-coordinate for the button's top-left corner.
-        :param y: Y-coordinate for the button's top-left corner.
-        :param image: The image surface for the button.
-        :param scale: Scaling factor for resizing the button image.
+        :param x_coord: X-coordinate for the button's top-left corner.
+        :param y_coord: Y-coordinate for the button's top-left corner.
+        :param img_surface: The image surface for the button.
+        :param scale_factor: Scaling factor for resizing the button image.
         """
-        super().__init__(x, y, image, scale)
+        super().__init__(x_coord, y_coord, img_surface, scale_factor)
         self.clicked = False
 
-    def check_if_button_is_pressed(self, screen):
+    def check_if_button_is_pressed(self):
         """
         Checks if the button is pressed, updates its state, and returns the action.
-
-        :param screen: The surface where the button will be checked and drawn.
         :return: True if the button is pressed, False otherwise.
         """
         action = False
@@ -182,7 +174,7 @@ class Bird(pygame.sprite.Sprite):
         :param x: X-coordinate for the bird's center.
         :param y: Y-coordinate for the bird's center.
         """
-        sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
         self.images = []
         self.index = 0
         self.counter = 0
@@ -209,7 +201,6 @@ class Bird(pygame.sprite.Sprite):
         if not game_over:
             # Jump up if user uses mouse left click
             left_click = mouse.get_pressed()[0]
-
             if left_click == 1 and not self.clicked:
                 self.clicked = True
                 self.vel = -10
@@ -217,7 +208,7 @@ class Bird(pygame.sprite.Sprite):
             if left_click == 0:
                 self.clicked = False
 
-            # Bird moving animation
+            # Bird flapping animation
             self.counter += 1
             flap_cooldown = 5
             if self.counter > flap_cooldown:
@@ -225,14 +216,13 @@ class Bird(pygame.sprite.Sprite):
                 self.index += 1
                 if self.index >= len(self.images):
                     self.index = 0
-            self.image = self.images[self.index]
 
             # Rotate the bird
-            self.image = transform.rotate(self.images[self.index], self.vel * -2)
+            self.image = transform.rotate(surface=self.images[self.index], angle=self.vel * -2)
 
         else:
             # Make the bird face down when it falls to ground / collides with pipe
-            self.image = transform.rotate(self.images[self.index], -90)
+            self.image = transform.rotate(surface=self.images[self.index], angle=-90)
 
 
 class Pipe(pygame.sprite.Sprite):
@@ -244,12 +234,12 @@ class Pipe(pygame.sprite.Sprite):
         :param y: Y-coordinate for the pipe's center.
         :param position: 1 for top pipe, -1 for bottom pipe.
         """
-        sprite.Sprite.__init__(self)
+        pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("Resources/pipe.png")
         self.rect = self.image.get_rect()
         pipe_gap_half = PIPE_GAP // 2
         if position == 1:
-            self.image = transform.flip(self.image, False, True)
+            self.image = transform.flip(surface=self.image, flip_x=False, flip_y=True)
             self.rect.bottomleft = (x, y - pipe_gap_half)
         if position == -1:
             self.rect.topleft = (x, y + pipe_gap_half)
@@ -274,7 +264,7 @@ bird_group.add(bird)
 # Create UI buttons
 title = Element(SCREEN_WIDTH_HALF - 270, 30, title_img, 2)
 restart_btn = Button(SCREEN_WIDTH_HALF - 80, SCREEN_HEIGHT_HALF - 100, restart_img, 1.5)
-restart_exit_btn = Button(SCREEN_WIDTH_HALF - 50, SCREEN_HEIGHT_HALF, exit_img, 0.5)
+restart_menu_exit_btn = Button(SCREEN_WIDTH_HALF - 50, SCREEN_HEIGHT_HALF, exit_img, 0.5)
 start_btn = Button(SCREEN_WIDTH_HALF - 130, SCREEN_HEIGHT_HALF - 130, start_img, 0.8)
 exit_btn = Button(SCREEN_WIDTH_HALF - 110, SCREEN_HEIGHT_HALF + 50, exit_img, 0.8)
 resume_btn = Button(SCREEN_WIDTH_HALF - 105, SCREEN_HEIGHT_HALF - 150, resume_img, 0.8)
@@ -288,25 +278,26 @@ while running:
     # Display start menu
     if not start_game:
         draw_background_and_ground()
-        title.draw(screen)
-        start_btn.draw(screen)
-        exit_btn.draw(screen)
+        title.draw()
+        start_btn.draw()
+        exit_btn.draw()
 
-        if start_btn.check_if_button_is_pressed(screen):
+        if start_btn.check_if_button_is_pressed():
             start_game = True
-        if exit_btn.check_if_button_is_pressed(screen):
+            flying = True
+        if exit_btn.check_if_button_is_pressed():
             save_high_score(high_score)
             running = False
 
     # Display pause menu
     elif game_paused and not game_over and flying:
         draw_background_and_ground()
-        resume_btn.draw(screen)
-        pause_menu_exit_btn.draw(screen)
+        resume_btn.draw()
+        pause_menu_exit_btn.draw()
         
-        if resume_btn.check_if_button_is_pressed(screen):
+        if resume_btn.check_if_button_is_pressed():
             game_paused = False
-        if pause_menu_exit_btn.check_if_button_is_pressed(screen):
+        if pause_menu_exit_btn.check_if_button_is_pressed():
             save_high_score(high_score)
             running = False
 
@@ -315,56 +306,14 @@ while running:
         screen.blit(background_img, (0, 0))
 
         # Draw bird
-        bird_group.draw(screen)
+        bird_group.draw(surface=screen)
         bird_group.update()
 
         # Draw pipes
-        pipe_group.draw(screen)
+        pipe_group.draw(surface=screen)
 
         # Draw ground
         screen.blit(ground_img, (ground_scroll, 768))
-
-        # Check if bird passed the pipe, update score and high score (if needed).
-        if len(pipe_group) > 0:
-            if (
-                bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left
-                and bird_group.sprites()[0].rect.right
-                < pipe_group.sprites()[0].rect.right
-                and not pipe_passed
-            ):
-                pipe_passed = True
-
-            if pipe_passed:
-                if (
-                    bird_group.sprites()[0].rect.left
-                    > pipe_group.sprites()[0].rect.right
-                ):
-                    score += 1
-                    if score > high_score:
-                        high_score = score
-                    mixer.Sound.play(scored)
-                    pipe_passed = False
-
-            render_score_and_high_score(score, high_score)
-
-        # Check for collision between bird and pipe
-        if (
-            sprite.groupcollide(bird_group, pipe_group, False, False)
-            or bird.rect.top < 0
-        ):
-            collided += 1
-            play_thump = True
-            game_over = True
-            if collided == 1:
-                mixer.Sound.play(thump)
-
-        # Check if bird fell to ground
-        if bird.rect.bottom >= 768:
-            fell += 1
-            game_over = True
-            flying = False
-            if fell == 1:
-                mixer.Sound.play(thump)
 
         # Generate new pipes if bird is flying
         if not game_over and flying:
@@ -387,40 +336,69 @@ while running:
             # Update pipes to screen
             pipe_group.update()
 
-        # Display game over menu 
+        # Check if bird passed the pipe, update score and high score (if needed).
+        if len(pipe_group) > 0:
+            if (
+                bird_group.sprites()[0].rect.left > pipe_group.sprites()[0].rect.left
+                and bird_group.sprites()[0].rect.right
+                < pipe_group.sprites()[0].rect.right
+                and not pipe_passed
+            ):
+                pipe_passed = True
+
+            if pipe_passed:
+                if (
+                    bird_group.sprites()[0].rect.left
+                    > pipe_group.sprites()[0].rect.right
+                ):
+                    score += 1
+                    if score > high_score:
+                        high_score = score
+                    scored.play()
+                    pipe_passed = False
+
+            render_score_and_high_score(score, high_score)
+
+        # Check for collision between bird and pipe
+        if (
+            sprite.groupcollide(groupa=bird_group, groupb=pipe_group, dokilla=False, dokillb=False)
+            or bird.rect.top < 0
+        ):
+            collided += 1
+            game_over = True
+            if collided == 1:
+                thump.play()
+
+        # Check if bird fell to ground
+        if bird.rect.bottom >= 768:
+            fell += 1
+            game_over = True
+            flying = False
+            if fell == 1:
+                thump.play()
+
+        # Display game over menu
         if game_over:
-            restart_btn.draw(screen)
-            restart_exit_btn.draw(screen)
-            if restart_btn.check_if_button_is_pressed(screen):
+            restart_btn.draw()
+            restart_menu_exit_btn.draw()
+            if restart_btn.check_if_button_is_pressed():
                 game_over = False
                 fell = 0
                 collided = 0
                 score = reset_game()
-            if restart_exit_btn.check_if_button_is_pressed(screen):
+            if restart_menu_exit_btn.check_if_button_is_pressed():
                 save_high_score(high_score)
                 running = False
 
     # Check for events
     for event in events.get():
-        if (
-            event.type == pygame.QUIT or
-            event.type == pygame.KEYDOWN
-            and event.key == pygame.K_ESCAPE
-        ):
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             running = False
 
-        if (
-            event.type == pygame.MOUSEBUTTONDOWN
-            and flying is False
-            and game_over is False
-        ):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not flying and not game_over:
             flying = True
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                game_paused = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            game_paused = True
 
     pygame.display.update()
-
-
-pygame.quit()
